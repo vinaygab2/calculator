f4eea6e419b351b7153e6a7c96c49b7ae03dc33f
vinay <vinkjaiswal@gmail.com>
2025-05-05 12:59:38 +0530

    Add operate function in script.js Operate function calls other arithmetic functions based on operator

--- script.js
+++ script.js
@@ -2,3 +2,37 @@ const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
+
+let num1, num2, operator;
+
+const operate = (num1, num2, operator) => {
+  switch (operator) {
+    case '+':
+      console.log(add(num1, num2));
+      break;
+
+    case '-':
+      console.log(subtract(num1, num2));
+      break;
+
+    case '*':
+      console.log(multiply(num1, num2));
+      break;
+
+    case '/':
+      console.log(divide(num1, num2));
+      break;
+
+    default:
+      console.log('Please enter a valid operator!');
+      break;
+  }
+};
+
+switch (key) {
+  case value:
+    break;
+
+  default:
+    break;
+}
